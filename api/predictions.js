84ed71ea39b9b8087c3d098a6c3c6b7c0756a481
gonzaloty09-ux <gonzaloty09@gmail.com>
2026-09-18 12:47:47 -0400

    Update predictions.js

--- api/predictions.js
+++ api/predictions.js
@@ -277,4 +277,4 @@ export default async function handler(req, res) {
   } catch (e) {
     res.status(500).json({ ok: false, error: String(e.message || e) });
   }
-                                                                  }
+}
